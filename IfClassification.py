62b64fef83ce5f739aef1696c28c374f7f7fff86
LNBaud <44223448+LNBaud@users.noreply.github.com>
2018-10-31 11:36:57 +0100

    ajout de .py dans le nom du fichier + resolution du problème de read

---
+++ IfClassification.py
@@ -0,0 +1,61 @@
+import logging
+from datetime import datetime, timezone
+from config import FILEDIR, FILEBREAK
+import re
+
+
+class IfClassification:
+
+    def __init__(self):
+
+        self.current_file = 'C:\\Users\\Hélène\\Downloads\\tweets_2018-10-31T09_24_43.923494.txt'
+        self.nf_line_count =0
+        self.new_file = FILEDIR + "tweets_" + datetime.now().strftime("%Y-%m-%dT%H:%M:%S.%f") + ".txt"
+
+
+    def open_write(self):
+        with open(self.current_file, "r", encoding='utf-8') as f:
+            data = f.readlines()
+            #print (data)
+            for i in range (1,len(data)) :
+                ligne = data [i]
+                data_split = ligne.split(',')
+                print (data_split)
+                self.nb_follower = re.sub("'",'', data_split[0])
+                self.nb_following = re.sub("'",'', data_split[1])
+                self.verified = re.sub("'",'', data_split[2])
+                self.reputation = re.sub("'",'', data_split[3])
+                self.age = re.sub("'",'', data_split[4])
+                self.nb_tweets = re.sub("'",'', data_split[5])
+                self.proportion_spamwords= re.sub("'",'', data_split[6])
+                self.orthographe= re.sub("'",'', data_split[7])
+                self.nb_emoji= re.sub("'",'', data_split[8])
+                self.RT = re.sub("'",'', data_split[9])
+                self.spam = re.sub("['\\n]",'', data_split[10])
+                print(self.spam)
+                with open(self.new_file, "a+", encoding='utf-8') as nf:
+                    nf.write(data + classification(self) + "\n")
+                self.nf_line_count += 1
+
+                if self.nf_line_count > FILEBREAK:
+                    logging.info("Closing file {}".format(self.current_file))
+                    self.current_file = FILEDIR + "tweets_" + datetime.now().strftime("%Y-%m-%dT%H:%M:%S.%f") + ".txt"
+                    self.nf_line_count = 0
+
+
+
+    def classification(self):
+        potentialspam = true
+        verdict =0
+        if self.nb_emoji == 0 :
+            if self.proportion_spamword < 0.1 :
+                if self.orthgraphe > 0.5 :
+                    if RT == True :
+                        potentialspam = "false"
+        if potentialspam == self.spam :
+            verdict =1
+        return potentialspam + ","+ verdict
+
+
+fichier = IfClassification()
+fichier.open_write()
