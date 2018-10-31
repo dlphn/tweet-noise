c155fac60a625d88aec5b700f17a7c9b243c417f
Delphine Shi <shi.delphine@gmail.com>
2018-10-31 12:01:44 +0100

    Fix issues in IfClassification

--- IfClassification.py
+++ IfClassification.py
@@ -21,19 +21,19 @@ class IfClassification:
                 ligne = data [i]
                 data_split = ligne.split(',')
                 print (data_split)
-                self.nb_follower = re.sub("'",'', data_split[0])
-                self.nb_following = re.sub("'",'', data_split[1])
-                self.verified = re.sub("'",'', data_split[2])
-                self.reputation = re.sub("'",'', data_split[3])
-                self.age = re.sub("'",'', data_split[4])
-                self.nb_tweets = re.sub("'",'', data_split[5])
-                self.time = re.sub("'",'', data_split[6])
-                self.proportion_spamwords= re.sub("'",'', data_split[7])
-                self.orthographe= re.sub("'",'', data_split[8])
-                self.nb_emoji= re.sub("'",'', data_split[9])
-                self.RT = re.sub("'",'', data_split[10])
-                self.spam = re.sub("['\\n]",'', data_split[11])
-                print(self.spam)
+                self.id = re.sub("'",'', data_split[0])
+                self.nb_follower = re.sub("'",'', data_split[1])
+                self.nb_following = re.sub("'",'', data_split[2])
+                self.verified = re.sub("'",'', data_split[3])
+                self.reputation = re.sub("'",'', data_split[4])
+                self.age = re.sub("'",'', data_split[5])
+                self.nb_tweets = re.sub("'",'', data_split[6])
+                self.time = re.sub("'",'', data_split[7])
+                self.proportion_spamwords= re.sub("'",'', data_split[8])
+                self.orthographe= re.sub("'",'', data_split[9])
+                self.nb_emoji= re.sub("'",'', data_split[10])
+                self.RT = re.sub("'",'', data_split[11])
+                self.spam = re.sub("[\"'\\n]", '', data_split[12])
                 with open(self.new_file, "a+", encoding='utf-8') as nf:
                     nf.write(ligne + self.classification() + "\n")
                 self.nf_line_count += 1
@@ -46,13 +46,14 @@ class IfClassification:
     def classification(self):
         potentialspam = "true"
         verdict =0
-        if self.nb_emoji == 0 :
-            if self.proportion_spamword < 0.1 :
-                if self.orthgraphe > 0.5 :
-                    if RT == True :
+        if int(self.nb_emoji) == 0 :
+            if float(self.proportion_spamwords) < 0.1 :
+                if float(self.orthographe) > 0.5 :
+                    if self.RT == "\"true\"":
                         potentialspam = "false"
-        if potentialspam == self.spam :
-            verdict =1
+        if potentialspam == self.spam:
+            verdict = 1
+        print(verdict)
         return potentialspam + ","+ str(verdict)
 
 
