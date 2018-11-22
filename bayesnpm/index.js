efe51f474589cc0434a7d422af71a8413d3c6f50
Delphine Shi <shi.delphine@gmail.com>
2018-11-22 10:55:51 +0100

    Train Bayes npm on all tweets

--- bayesnpm/index.js
+++ bayesnpm/index.js
@@ -1,2 +1,19 @@
+
 var bayes = require('bayes');
 var classifier = bayes();
+var spamTweets = require("/Users/delphineshi/Downloads/temp/tweets_spam.json");
+var infoTweets = require("/Users/delphineshi/Downloads/temp/tweets_info.json");
+
+const data = [{name: 'info', tweets: infoTweets}, {name: 'spam', tweets: spamTweets}];
+
+for (var category of data) {
+  console.log(`training model with ${category.name} data.`)
+  for (var tweet of category.tweets) {
+    classifier.learn(tweet.text, category.name);
+  }
+}
+
+
+console.log(classifier.categorize("Brexit : La Grande-Bretagne, grande perdante")); //info
+console.log(classifier.categorize("Top cool j'adore !")); //spam
+console.log(classifier.categorize("Supportons les gilets jaunes, la hausse des carburants est intolérable")); //info
