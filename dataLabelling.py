c9b5e55731aaa7bdf0a47755b5b90a455d2639d4
LNBaud <44223448+LNBaud@users.noreply.github.com>
2018-12-19 10:17:02 +0100

    type de spam

--- dataLabelling.py
+++ dataLabelling.py
@@ -30,17 +30,21 @@ class DataLabelling:
         Retrieve tweets from mongo database and save user's label for the tweets.
         """
         print("=============================================\n")
-        print("Tweets labelling - possible inputs: y/yes, n/no, skip/next/pass, stop/end, help.\nIs the tweet displayed considered as spam?\n")
+        print("Tweets labelling - possible inputs: bot/b, conv/c, pub/p, actu/a, reaction/r, other, skip/next/pass, "
+              "stop/end, help.\nWhat is the type of the tweet displayed?\n")
         print("=============================================\n")
         for obj in self.db.tweets.find():
             try:
-                obj["spam"]
+                obj["type"]
             except KeyError:
                 classification = self.label(obj)
                 if classification == "stop" or classification == "end" or classification == "x":
                     break
-                elif isinstance(classification, bool):
-                    self.db.tweets.update_one({"_id": obj.get("_id")}, {"$set": {"spam": classification}})
+                elif classification in ["bot","conversation","publicité", "other spam", "actualité","reaction"]:
+                    spam_value = True
+                    if classification in ["actualité","reaction"] :
+                        spam_value = False
+                    self.db.tweets.update_one({"_id": obj.get("_id")}, {"$set": {"type": classification,"spam":spam_value}})
                     self.count += 1
         logging.info("Total of {} elements labelled".format(self.count))
 
@@ -52,19 +56,20 @@ class DataLabelling:
         :return: the user's answer (True, False, or an action to skip or stop)
         """
         # display tweet and allow input from user true/false
-        valid = {"yes": True, "y": True, "ye": True, "no": False, "n": False}
+        valid = {"bot" : "bot", "b" : "bot", "conv" : "conversation", "c" : "conversation", "pub": "publicité",
+                 "p": "publicité", "other" : "other spam", "actu" : "actualité","a" : "actualité", "reaction" : "reaction", "r" : "reaction"}
         other_actions = ["stop", "end", "x", "skip", "next", "pass"]
         while True:
             print("https://twitter.com/test/status/" + data["id_str"] + " : " + data["text"])
-            choice = input("Spam? [y/n]\n").lower()
+            choice = input("Type? [bot/conv/pub/actu/reaction or other]\n").lower()
             if choice in valid:
                 return valid[choice]
             elif choice in other_actions:
                 return choice
             elif choice == "help":
-                print("Possible inputs: y/yes, n/no, skip/next/pass, stop/end, help")
+                print("Possible inputs: bot/b, conv/c, pub/p, actu/a, reaction/r, other, skip/next/pass, stop/end, help")
             else:
-                print("Please respond with 'yes' or 'no' (or 'y' or 'n').\n")
+                print("Please respond with 'bot'/'b', 'conv'/'c', 'pub'/'p', 'actu'/'a', 'reaction'/'r' or 'other' .\n")
 
 
 if __name__ == "__main__":
