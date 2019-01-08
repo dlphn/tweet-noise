34269ee2b092dde304b47bee87db57191bff70f3
Delphine Shi <shi.delphine@gmail.com>
2019-01-08 21:33:47 +0100

    Test on French sentences

--- textClustering.py
+++ textClustering.py
@@ -29,13 +29,14 @@ class TextClustering:
     def cluster_texts(self, texts, k=3):
         """ Transform texts to Tf-Idf coordinates and cluster texts using K-Means """
         self.vectorizer = TfidfVectorizer(tokenizer=self.process_text,
-                                          stop_words=stopwords.words('english'),
+                                          # stop_words=stopwords.words('english'),
+                                          stop_words=stopwords.words('french'),
                                           max_df=0.5,
                                           min_df=0.1,
                                           lowercase=True)
 
         tfidf_model = self.vectorizer.fit_transform(texts)
-        self.km_model = KMeans(n_clusters=clusters)
+        self.km_model = KMeans(n_clusters=k)
         self.km_model.fit(tfidf_model)
 
         clustering = collections.defaultdict(list)
@@ -61,16 +62,28 @@ class TextClustering:
 
 
 if __name__ == "__main__":
-    articles = ["This little kitty came to play when I was eating at a restaurant.",
-                "Merley has the best squooshy kitten belly.",
-                "Google Translate app is incredible.",
-                "If you open 100 tab in google you get a smiley face.",
-                "Best cat photo I've ever taken.",
-                "Climbing ninja cat.",
-                "Impressed with google map feedback.",
-                "Key promoter extension for Google Chrome."]
+    # articles = ["This little kitty came to play when I was eating at a restaurant.",
+    #             "Merley has the best squooshy kitten belly.",
+    #             "Google Translate app is incredible.",
+    #             "If you open 100 tab in google you get a smiley face.",
+    #             "Best cat photo I've ever taken.",
+    #             "Climbing ninja cat.",
+    #             "Impressed with google map feedback.",
+    #             "Key promoter extension for Google Chrome."]
+    # model = TextClustering()
+    # clusters = model.cluster_texts(articles, 2)
+    # print(dict(clusters))
+    # model.predict("chrome browser to open.")
+    # model.predict("My cat is hungry.")
+
+    articles = ["J'aime les frites",
+                "Je suis d'accord, j'aime pas la pluie...",
+                "Les frites je les adore",
+                "La taxe d'habitation ne sera pas augmentée annonce le gouvernement",
+                "Cool pas d'augmentation de la taxe d'habitation !!!",
+                "Il fait trop moche aujourd'hui", ]
     model = TextClustering()
-    clusters = model.cluster_texts(articles, 2)
+    clusters = model.cluster_texts(articles, 3)
     print(dict(clusters))
-    model.predict("chrome browser to open.")
-    model.predict("My cat is hungry.")
+    model.predict("Des manifestants dans la rue pour exprimer leur colère face à l'augmentation de la taxe d'habitation.")
+    model.predict("Lol je sors sans parapluie trankil et là il pleut.")
