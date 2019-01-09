61a43a643236d697a3adeb93800350d6dc671fdb
Delphine Shi <shi.delphine@gmail.com>
2019-01-09 14:43:20 +0100

    Better working clustering

--- textClustering.py
+++ textClustering.py
@@ -8,6 +8,7 @@ from sklearn.cluster import KMeans
 from sklearn.feature_extraction.text import TfidfVectorizer
 
 import arrayBuilder
+from featuresBuilder import stopKeywords
 
 
 class TextClustering:
@@ -15,6 +16,16 @@ class TextClustering:
     def __init__(self):
         self.vectorizer = None
         self.km_model = None
+
+    @staticmethod
+    def process_text_simple(text):
+        """ Tokenize text removing punctuation """
+        # stop = set(stopwords.words('french'))
+        stop = stopKeywords.keywords_stoplist
+        text = re.sub(r'(?:\@|https?\://)\S+', '', text)  # remove links and @username
+        text = re.sub(r'[^\w\s]', ' ', text)  # remove non-alphanumeric characters
+        text = ' '.join([token for token in word_tokenize(text.lower()) if token not in stop])  # remove stopwords
+        return text
 
     @staticmethod
     def process_text(text, stem=True):
@@ -31,12 +42,16 @@ class TextClustering:
 
     def cluster_texts(self, texts, k=3):
         """ Transform texts to Tf-Idf coordinates and cluster texts using K-Means """
-        self.vectorizer = TfidfVectorizer(tokenizer=self.process_text,
-                                          # stop_words=stopwords.words('english'),
-                                          stop_words=stopwords.words('french'),
-                                          max_df=0.8,
-                                          min_df=0.1,
-                                          lowercase=True)
+        self.vectorizer = TfidfVectorizer(
+            # tokenizer=self.process_text,
+            # stop_words=stopwords.words('english'),
+            # stop_words=stopwords.words('french'),
+            # max_df=0.8,
+            # min_df=0.1,
+            # lowercase=True
+        )
+        texts = [self.process_text_simple(text) for text in texts]
+        print(texts)
 
         tfidf_model = self.vectorizer.fit_transform(texts)
         self.km_model = KMeans(n_clusters=k, init='k-means++', n_init=10)
@@ -100,9 +115,12 @@ if __name__ == "__main__":
     # model.predict("Lol je sors sans parapluie trankil et là il pleut.")
 
     # Tweets
-    articles = get_tweets()[:15]
+    articles = get_tweets()
     print(articles)
     model = TextClustering()
-    clusters = model.cluster_texts(articles, 2)
-    print(dict(clusters))
+    clusters = model.cluster_texts(articles, 15)
+    result = dict(clusters)
+    print(result)
+    for key in result.keys():
+        print(key, len(result[key]))
 
