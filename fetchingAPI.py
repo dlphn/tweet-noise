ab1974300f172680a9a878790ce0375e97a7dc66
Delphine Shi <shi.delphine@gmail.com>
2019-01-23 12:02:44 +0100

    Update db tweets with retweet count and favorite count

--- fetchingAPI.py
+++ fetchingAPI.py
@@ -1,108 +1,72 @@
 # -*- coding: utf-8 -*-
-"""
-Created on Tue May 17 17:31:33 2016
 
-@author: bmazoyer, dshi, hbaud, vlefranc
-"""
-# import json
+import time
 import logging
-import requests
-# from datetime import datetime
-from twython import Twython
-from config import ACCESS, PROXY, LANG, MONGODB
+from datetime import datetime, timedelta, timezone
+from twython import Twython, exceptions
+from config import ACCESS, MONGODB
 from pymongo import MongoClient
 
-# connect to MongoDB
-# client = MongoClient("mongodb://localhost:27017/")
-client = MongoClient("mongodb+srv://" + MONGODB["USER"] + ":" + MONGODB["PASSWORD"] + "@" + MONGODB["HOST"] + "/" + MONGODB["DATABASE"] + "?retryWrites=true")
-db = client[MONGODB["DATABASE"]]
 
 logging.basicConfig(format='%(asctime)s - %(levelname)s : %(message)s', level=logging.INFO)
 
 
 class FetchTweets:
     """
-    Retrieve data from the Twitter Streaming API.
-
-    The streaming API requires
-    `OAuth 1.0 <http://en.wikipedia.org/wiki/OAuth>`_ authentication.
+    Add latest numbers of retweets and favorites to the Mongo DB tweets.
     """
 
     def __init__(self, app_key, app_secret, oauth_token, oauth_token_secret):
 
         self.twitter = Twython(app_key, app_secret,
                                oauth_token, oauth_token_secret)
+        client = MongoClient(
+            "mongodb+srv://" + MONGODB["USER"] + ":" + MONGODB["PASSWORD"] + "@" + MONGODB["HOST"] + "/" + MONGODB[
+                "DATABASE"] + "?retryWrites=true")
+        self.db = client[MONGODB["DATABASE"]]
+        self.count = 0
 
-        # self.do_continue = True
-        # self.count = 0
-        # self.line_count = 0
-        # # self.current_file = FILEDIR + datetime.now().strftime("%Y-%m-%dT%H:%M:%S.%f") + ".txt"
-        # if PROXY:
-        #     client_args = {
-        #         'proxies': PROXY
-        #     }
-        # else:
-        #     client_args = {}
-        # TwythonStreamer.__init__(self, app_key, app_secret, oauth_token,
-        #                          oauth_token_secret, timeout=100, chunk_size=200,
-        #                          client_args=client_args)
-
-    # def on_success(self, data):
-    #     """
-    #     :param data: response from Twitter API (one tweet in json format)
-    #     """
-    #
-    #     if 'RT @' not in data["text"] :
-    #         db.tweets.insert_one(data)
-    #         self.count += 1
-    #         logging.info("Total of {} elements added".format(self.count))
-    #
-    #
-    #     # with open(self.current_file, "a+", encoding='utf-8') as f:
-    #     #     json.dump(data, f)
-    #     #     f.write("\n")
-    #     # self.line_count += 1
-    #     #
-    #     # if self.line_count > FILEBREAK:
-    #     #     logging.info("Closing file {}".format(self.current_file))
-    #     #     self.current_file = FILEDIR + datetime.now().strftime("%Y-%m-%dT%H:%M:%S.%f") + ".txt"
-    #     #     self.line_count = 0
-    #
-    #     if not self.do_continue:
-    #         logging.info("disconnect")
-    #         self.disconnect()
-    #
-    # def on_error(self, status_code, data):
-    #     """
-    #     :param status_code: The status code returned by the Twitter API
-    #     :param data: The response from Twitter API
-    #
-    #     """
-    #
-    #     logging.error(str(status_code) + ": " + str(data))
-
-    def sample(self, lang=None):
+    def retrieve(self):
         """
-        Wrapper for 'statuses / sample' API call
+        Retrieve more-than-one-week-old tweets from db
+        and update retweet_count and favorite_count
         """
+        start = time.time()
+        logging.info("Retrieving data...")
+        logging.info("Fetching tweets info...")
+        for obj in self.db.tweets.find():
+            try:
+                obj['updated_at']
+            except KeyError:
+                last_week = datetime.now(timezone.utc) - timedelta(days=7)
+                if datetime.strptime(obj['created_at'], '%a %b %d %H:%M:%S %z %Y') < last_week:
+                    retweets, favorites = self.fetch_info(obj['id_str'])
+                    print(retweets, favorites)
+                    self.db.tweets.update_one({"_id": obj["_id"]}, {
+                        "$set": {
+                            "updated_at": datetime.now(timezone.utc),
+                            "retweet_count": retweets,
+                            "favorite_count": favorites
+                        }
+                    })
+                    self.count += 1
+                    if self.count % 100 == 0:
+                        logging.info("{} elements updated".format(self.count))
 
-        # results = self.twitter.cursor(self.twitter.search, q='twitter', lang=lang, until='2019-01-16')
-        results = self.twitter.search(q='.', lang=lang, until='2019-01-16')
-        print(results)
-        for result in results['statuses']:
-        # for result in results:
-            print(result['id_str'], result['text'])
+        end = time.time()
+        logging.info("Total of {0} elements updated in {1} seconds".format(self.count, end - start))
 
-        # while self.do_continue:
-        #
-        # # Stream in an endless loop
-        #     try:
-        #         self.statuses.sample(language=lang)
-        #     except requests.exceptions.ChunkedEncodingError as e:
-        #         if e is not None:
-        #             logging.info("Encoding error (stream will continue): {0}".format(e))
+    def fetch_info(self, tweet_id):
+        """
+        Fetch tweet info
+        """
+        try:
+            tweet_info = self.twitter.show_status(id=tweet_id)
+            return tweet_info['retweet_count'], tweet_info['favorite_count']
+        except exceptions.TwythonError:
+            return 0, 0
 
 
 if __name__ == "__main__":
     twitter = FetchTweets(*ACCESS)
-    twitter.sample(lang=LANG)
+    twitter.retrieve()
