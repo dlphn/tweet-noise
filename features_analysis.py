8cf034944db79059910251c4c9110c93f10f99cb
Delphine Shi <shi.delphine@gmail.com>
2019-01-23 15:34:36 +0100

    Plot features

--- features_analysis.py
+++ features_analysis.py
@@ -1,4 +1,4 @@
-#from classification2 import Classification
+# from classification2 import Classification
 import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
@@ -7,25 +7,29 @@ from sklearn import decomposition
 from sklearn.preprocessing import StandardScaler
 import seaborn as sb
 
-#Data frame original
-current_file = "C:\\Users\\Public\\Documents\\tweets_2018-11-23T091721.577598.csv"
+from config import FILEDIR
+
+# Data frame original
+current_file = FILEDIR + "tweets_2019-01-23T14:26:33.598574.csv"
 dataframe = pd.read_csv(current_file, encoding="utf-8")
-#print(dataframe.head())
 
 
-#Normalisation du df
+# print(dataframe.head())
+
+
+# Normalisation du df
 def categorize_bool(x):
-    if x :
+    if x:
         return 1
-    else :
+    else:
         return 0
 
 
-#print(dataframe.verified)
+# print(dataframe.verified)
 dataframe.spam = dataframe.spam.apply(categorize_bool)
-df2 = pd.concat([dataframe.iloc[:,1:7], dataframe.iloc[:,8:]], axis=1)
-#print(dataframe.columns.tolist())
-#print(dataframe.describe())
+df2 = pd.concat([dataframe.iloc[:, 1:7], dataframe.iloc[:, 8:]], axis=1)
+# print(dataframe.columns.tolist())
+# print(dataframe.describe())
 
 """for i in range (5):
     df2.iloc[:,i] = ((df2.iloc[:,i] - df2.iloc[:,i].mean()) / (df2.iloc[:,i].max() - df2.iloc[:,i].min()))
@@ -41,21 +45,21 @@ for column_index, column in enumerate(df2.columns):
 plt.show()
 
 
-#Data frame classifié
-#classif = Classification()
-#dataframe = classif.create_dataframe()
+# Data frame classifié
+# classif = Classification()
+# dataframe = classif.create_dataframe()
 
 def show_features(dataset):
     columns = dataset.columns.values.tolist()
-    y = dataset.iloc[:,-1]
-    j=0
-    for i in [1,2,4,5] :
+    y = dataset.iloc[:, -1]
+    j = 0
+    for i in [1, 2, 4, 5]:
         j += 1
-        plt.subplot(2,2,j)
-        if j == 1 :
-            plt.axis(xmin =0, xmax = 500000)
-        if j == 2 :
-            plt.axis(xmax = 4000)
+        plt.subplot(2, 2, j)
+        if j == 1:
+            plt.axis(xmin=0, xmax=500000)
+        if j == 2:
+            plt.axis(xmax=4000)
         X = dataframe.iloc[:, i]
         plt.scatter(X, y)
         plt.xlabel('Paramètre {}'.format(columns[i]))
@@ -63,45 +67,32 @@ def show_features(dataset):
     plt.show()
 
     j = 0
-    for i in [6,7] :
+    for i in [6, 7]:
         j += 1
         X = dataframe.iloc[:, i]
-        plt.subplot(2,1,j)
-        if i == 6 :
-            plt.axis(xmax = 100000)
+        plt.subplot(2, 1, j)
+        if i == 6:
+            plt.axis(xmax=100000)
         plt.scatter(X, y)
         plt.xlabel('Paramètre {}'.format(columns[i]))
         plt.ylabel('Spam')
     plt.show()
 
-    i=0
-    for n in [3,8,9,10] :
-        i +=1
+    i = 0
+    for n in [3, 8, 9, 10]:
+        i += 1
         x = dataframe.iloc[:, n]
         c = Counter(zip(x, y))
-        a = Counter ( y )
-        s = [ 60*c[(xx, yy)]/a [(yy)] for xx, yy in zip(x, y)]
-        plt.subplot(2,2,i)
+        a = Counter(y)
+        s = [60 * c[(xx, yy)] / a[(yy)] for xx, yy in zip(x, y)]
+        plt.subplot(2, 2, i)
         plt.scatter(x, y, s=s)
         plt.xlabel('Paramètre {}'.format(columns[n]))
         plt.ylabel('Spam')
     plt.show()
 
     i = 0
-    for n in [11,12,13] :
-        i +=1
-        x = dataframe.iloc[:, n]
-        c = Counter(zip(x, y))
-        a = Counter ( y )
-        s = [ 30*c[(xx, yy)]/a [(yy)] for xx, yy in zip(x, y)]
-        plt.subplot(2,2,i)
-        plt.scatter(x, y, s=s)
-        plt.xlabel('Paramètre {}'.format(columns[n]))
-        plt.ylabel('Spam')
-    plt.show()
-
-    i = 0
-    for n in [11,12]:
+    for n in [11, 12, 13]:
         i += 1
         x = dataframe.iloc[:, n]
         c = Counter(zip(x, y))
@@ -113,24 +104,39 @@ def show_features(dataset):
         plt.ylabel('Spam')
     plt.show()
 
-def PCA(X,y):
-    #sc = StandardScaler()
-    #Z = sc.fit_transform(X)
-    #print(Z)
+    i = 0
+    for n in [11, 12]:
+        i += 1
+        x = dataframe.iloc[:, n]
+        c = Counter(zip(x, y))
+        a = Counter(y)
+        s = [30 * c[(xx, yy)] / a[(yy)] for xx, yy in zip(x, y)]
+        plt.subplot(2, 2, i)
+        plt.scatter(x, y, s=s)
+        plt.xlabel('Paramètre {}'.format(columns[n]))
+        plt.ylabel('Spam')
+    plt.show()
+
+
+def PCA(X, y):
+    # sc = StandardScaler()
+    # Z = sc.fit_transform(X)
+    # print(Z)
     pca = decomposition.PCA(n_components=3)
     principalComponents = pca.fit_transform(X)
     principalDf = pd.DataFrame(data=principalComponents
-                               , columns=['principal component 1', 'principal component 2','3'])
-    #print(principalDf)
+                               , columns=['principal component 1', 'principal component 2', '3'])
+    # print(principalDf)
     result = pd.concat([principalDf, y], axis=1)
     print(pca.explained_variance_ratio_)
     return result
 
+
 def correlation_matrix(dataset):
     columns = dataset.columns.values.tolist()
     correlations = dataset.corr()
-    #print(columns)
-    #print(correlations)
+    # print(columns)
+    # print(correlations)
     # plot correlation matrix
     fig = plt.figure()
     ax = fig.add_subplot(111)
@@ -143,28 +149,31 @@ def correlation_matrix(dataset):
     ax.set_yticklabels(columns)
     plt.show()
 
+
 def affichage(result):
-    mask = result.iloc[:,-1] == 0
-    negative = plt.scatter(result.iloc[:,0][~mask].values, result.iloc[:,1][~mask].values)
-    positive = plt.scatter(result.iloc[:,0][mask].values,result.iloc[:,1][mask].values)
+    mask = result.iloc[:, -1] == 0
+    negative = plt.scatter(result.iloc[:, 0][~mask].values, result.iloc[:, 1][~mask].values)
+    positive = plt.scatter(result.iloc[:, 0][mask].values, result.iloc[:, 1][mask].values)
     plt.xlabel('pca 1')
     plt.ylabel('pca 2')
     plt.legend((positive, negative), ('actu', 'non actu'))
     plt.show()
 
-def plot_features(dataframe,i,j) :
+
+def plot_features(dataframe, i, j):
     columns_name = dataframe.columns.values.tolist()
-    x = dataframe.iloc[:,i]
-    y = dataframe.iloc[:,j]
+    x = dataframe.iloc[:, i]
+    y = dataframe.iloc[:, j]
     c = Counter(zip(x, y))
     a = Counter(y)
     s = [30 * c[(xx, yy)] / a[(yy)] for xx, yy in zip(x, y)]
-    plt.scatter(x,y, s =s)
+    plt.scatter(x, y, s=s)
     plt.xlabel(columns_name[i])
     plt.ylabel(columns_name[j])
     plt.show()
 
-def visualisation_multi( dataframe):
+
+def visualisation_multi(dataframe):
     desag_finale = dataframe.columns.tolist()
     classes = list(map(int, desag_finale[:, -1]))
     nbre_classes = len(set(classes))
@@ -188,17 +197,17 @@ def visualisation_multi( dataframe):
                 plt.scatter(desag_finale[pp_k, arguments[x]], desag_finale[pp_k, arguments[y]], color=col)
 
 
-X = pd.concat([dataframe.iloc[:,3:7], dataframe.iloc[:,8:-1]], axis=1)
-#X = dataframe.iloc[:,1:-1]
-y = dataframe.iloc[:,-1]
-#print(X.head())
+X = pd.concat([dataframe.iloc[:, 3:7], dataframe.iloc[:, 8:-1]], axis=1)
+# X = dataframe.iloc[:,1:-1]
+y = dataframe.iloc[:, -1]
+# print(X.head())
 
-#df2 = dataframe[['nb_follower', 'nb_following', 'verified', 'nb_tweets', 'proportion_spamwords', 'proportion_whitewords',  'orthographe',  'nb_emoji']]
-#print(plot_features(df2,0,1))
+# df2 = dataframe[['nb_follower', 'nb_following', 'verified', 'nb_tweets', 'proportion_spamwords', 'proportion_whitewords',  'orthographe',  'nb_emoji']]
+# print(plot_features(df2,0,1))
 
-#print(correlation_matrix(X))
+# print(correlation_matrix(X))
 
-#a = PCA(df2,y)
-#affichage(a)
+# a = PCA(df2,y)
+# affichage(a)
 
-#show_features(dataframe)
+# show_features(dataframe)
