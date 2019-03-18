0c82048ddd2018758a11f089427d7ddc51be836d
Delphine Shi <shi.delphine@gmail.com>
2019-03-18 15:05:16 +0100

    Build clusters on training set

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 
 import logging
+import pandas as pd
+from sklearn.model_selection import train_test_split
 from tweetsClustering.src.load_data import load_data
+from tweetsClustering.src.clustering_algo import ClusteringAlgo
+from tweetsClustering.src.compute_tfidf import TfIdf
 
 logging.basicConfig(format='%(asctime)s - %(levelname)s : %(message)s', level=logging.INFO)
 
@@ -13,9 +17,30 @@ if __name__ == "__main__":
     logging.info("loaded {} tweets".format(len(data)))
 
     # Split data in train/test
-    print(data.head())
+    train, test = train_test_split(data, test_size=0.3)
+    train = train.copy()
+    test = test.copy()
 
     # Build clusters for train
+    t = 0.7
+    w = 100000
+    batch_size = 100
+    distance = "cosine"
+    embedding_day = "base_fr_2"
+    DATA_PATH = "tweetsClustering/data/"
+
+    clustering = ClusteringAlgo(threshold=t, window_size=w, batch_size=batch_size, distance=distance)
+    transformer = TfIdf()
+    count_vectors = transformer.load_history(DATA_PATH + embedding_day).add_new_samples(train)
+    vectors = transformer.compute_vectors(count_vectors, min_df=10)
+    clustering.add_vectors(vectors)
+    labels = clustering.incremental_clustering(method="brute")
+
+    unique_clusters = set(labels)
+    logging.info("Nb unique clusters: {}".format(len(unique_clusters)))
+
+    train["pred"] = pd.Series(labels, dtype=train.label.dtype)
+    print(train.head())
 
     # For each tweet in test :
     #   1. Predict cluster
