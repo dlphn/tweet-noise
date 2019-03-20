52d0a337cb9944749a638bf16dcf006a7d380fc3
LNBaud <44223448+LNBaud@users.noreply.github.com>
2019-03-20 09:06:27 +0100

    comment spam classifier

--- spamClassifier.py
+++ spamClassifier.py
@@ -29,27 +29,29 @@ class Classification:
 
     #On regarde les parametres du cluster pour voir si on peut classer directement le tweet comme spam
     def classify_bycluster(self):
-        #print(type(self.tweet))
         num_cluster = self.tweet['pred']
-        #print(num_cluster)
         if num_cluster == -1:
             cluster_class, rf_class = self.set_cluster_spam(True)
             return cluster_class,rf_class
         else :
             df_pred = self.df[self.df['pred']== num_cluster]
             length = df_pred.shape[0]
+            #Si le cluster fait 4 tweets ou moins on analyse le tweet grace à notre algo random forest
             if length < 4 :
                 cluster_class, rf_class = self.set_cluster_spam(False)
                 return cluster_class,rf_class
             else :
                 df_pred.label.apply(self.categorize_label)
+                #Si le cluster fait plus de 4 tweets et que dedans plus de 10% est de l'actualité on analyse le tweet
                 if df_pred.label.sum(axis = 0)/length > 0.1 :
                     cluster_class, rf_class = self.set_cluster_spam(False)
                     return cluster_class,rf_class
                 else :
+                    #Si le cluster fait plus de 4 tweets, a moins de 10% d'actu mais contient un media dedans on analyse le tweet
                     if self.contains_media(df_pred) :
                         cluster_class, rf_class = self.set_cluster_spam(False)
                         return cluster_class,rf_class
+                    #Si le cluster fait plus de 4 tweet, contient plus de 90% de spam et aucun media on le classe automatiquement spam
                     else :
                         cluster_class, rf_class = self.set_cluster_spam(True)
                         return cluster_class,rf_class
